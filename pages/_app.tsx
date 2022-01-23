8e414575a55bc4f079606606ee58a169cb9bdac1
itizawa <itizawa.pen@gmail.com>
2022-01-23 17:47:19 +0900

    ✨ display header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="年号コンバーター" />
+        <meta property="og:description" content="年号を入力すると生まれ年の変換をしてくれます" />
+        <meta property="og:url" content="https://conversion-e6b5e.firebaseapp.com/" />
+        <meta property="og:image" content="img/og.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@itizawa_pen" />
+        <meta name="twitter:creator" content="@itizawa_pen" />
+
+        <title>元号コンバーター</title>
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
