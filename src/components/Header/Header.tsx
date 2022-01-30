1251e01c27312bb6cde0c2140ceaf80c5972f4e9
itizawa <itizawa.pen@gmail.com>
2022-01-31 00:05:10 +0900

    💄 style header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,31 @@
 import { VFC } from 'react';
+import styled from 'styled-components';
 
 /**
  * Header
  */
 export const Header: VFC = () => {
   return (
-    <header>
-      <h1>元号コンバーター</h1>
-      <p>あなたは明治〇〇年生まれ！？</p>
-    </header>
+    <StyledHeader>
+      <StyledHeader1>元号コンバーター</StyledHeader1>
+      <StyledParagraph>あなたは明治〇〇年生まれ！？</StyledParagraph>
+    </StyledHeader>
   );
 };
+
+const StyledHeader = styled.header`
+  background: #000080;
+  color: white;
+  box-shadow: 0px 2px 3px gray;
+`;
+
+const StyledHeader1 = styled.h1`
+  padding: 20px 0px;
+  font-weight: bold;
+  font-size: 32px;
+`;
+
+const StyledParagraph = styled.p`
+  padding: 4px 0px;
+  background: #47abf4;
+`;
