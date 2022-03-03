e46a9711163b84b4f4b127bd21c88c04c2796c55
itizawa <itizawa.pen@gmail.com>
2022-03-04 00:32:58 +0900

    ✨ display EraCard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from 'next';
 import { useCallback, useState } from 'react';
 import styled from 'styled-components';
 import { EraCard, Header } from '~/components';
-import { TweetButton } from '~/components/TweetButton/TweetButton';
 // import { getYear } from 'date-fns';
 
 const Home: NextPage = () => {
@@ -42,67 +41,16 @@ const Home: NextPage = () => {
           <button onClick={handleClickConversionButton}>Conversion!!</button>
         </div>
         {showResult && (
-          <>
+          <div style={{ display: 'flex', flexDirection: 'column', gridRowGap: '24px' }}>
             <EraCard era="令和" years={1} reason="今上天皇の譲位" />
-            <li className="box">
-              <h2>
-                令和 <span className="result" id="N"></span> 年生まれです！
-              </h2>
-              <p>
-                （天皇名未定）
-                <br />
-                改元理由：今上天皇の譲位
-              </p>
-              <TweetButton onClick={() => console.log('hoge')} />
-            </li>
-            <li className="box">
-              <h2>
-                平成 <span className="result" id="H"></span> 年生まれです！
-              </h2>
-              <p>
-                今上天皇の時代です
-                <br />
-                改元理由：今上天皇即位による
-              </p>
-              <TweetButton onClick={() => console.log('hoge')} />
-            </li>
-            <li className="box">
-              <h2>
-                昭和 <span className="result" id="S"></span> 年生まれです！
-              </h2>
-              <p>
-                昭和天皇の時代です
-                <br />
-                改元理由：昭和天皇践祚による
-              </p>
-              <TweetButton onClick={() => console.log('hoge')} />
-            </li>
-            <li className="box">
-              <h2>
-                大正 <span className="result" id="T"></span> 年生まれです！
-              </h2>
-              <p>
-                大正天皇の時代です
-                <br />
-                改元理由：大正天皇践祚による
-              </p>
-              <TweetButton onClick={() => console.log('hoge')} />
-            </li>
-            <li className="box">
-              <h2>
-                明治 <span className="result" id="M"></span> 年生まれです！
-              </h2>
-              <p>
-                明治天皇の時代です
-                <br />
-                改元理由：明治天皇践祚による
-              </p>
-              <TweetButton onClick={() => console.log('hoge')} />
-            </li>
-          </>
+            <EraCard era="平成" years={1} name="今上天皇" reason="今上天皇即位による" />
+            <EraCard era="昭和" years={1} name="昭和天皇" reason="昭和天皇践祚による" />
+            <EraCard era="大正" years={1} name="大正天皇" reason="大正天皇践祚による" />
+            <EraCard era="明治" years={1} name="明治天皇" reason="明治天皇践祚による" />
+          </div>
         )}
 
-        <p>直近の元号に変換したものが表示されます!</p>
+        <p style={{ marginTop: '32px' }}>直近の元号に変換したものが表示されます!</p>
 
         <h4>
           ※当サイトの情報に誤りがある場合があります。
